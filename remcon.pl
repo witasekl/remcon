fceb5b89b626a8050b044ec6261ad633e465b956
Libor Witasek <witasekl@gmail.com>
2012-03-04 17:44:50 +0100

    Add detection of attached GNU Screen.
    - Allows user to disable auto-away functionality and message forwarding,
      if GNU Screen is attached.

--- remcon.pl
+++ remcon.pl
@@ -46,6 +46,7 @@ use constant {
 
 my $event_privmsg_ref = \&event_privmsg; 
 my $nicklist_remove_ref = \&nicklist_remove;
+my $socket = screen_socket_path();
 my $status;
 my $last_target;
 my $listen_root;
@@ -53,7 +54,45 @@ my $remcon_admin;
 my $remcon_channel;
 my $remcon_root;
 my $remcon_autoaway_msg;
+my $remcon_detect_attached_screen;
 my @awaylog;
+
+sub screen_socket_path{
+    my @screen_ls = `LC_ALL="C" screen -ls`;
+    my $screen_ls_length = @screen_ls;
+    my $sty = $ENV{'STY'};
+
+    if ($screen_ls_length > 1 and $sty) {
+        my $socket = $screen_ls[$screen_ls_length - 2];
+        $socket = substr($socket, 0, length($socket) - 1);
+        if ($socket =~ s/^.* ([^ .]+)\.*$/\1/) {
+            return $socket . "/" . $sty;
+        }
+    }
+    return "";
+}
+
+sub is_screen_attached{
+    if ($socket) {
+        my @screen = stat($socket);
+        return (($screen[2] & 00100) != 0);
+    }
+    return 0;
+}
+
+sub only_if_detached_screen {
+    if ($remcon_detect_attached_screen) {
+        if ($socket) {
+            return !is_screen_attached();
+        }
+        else { # Not running in Screen
+            return 1;
+        }
+    }
+    else {
+        return 1;
+    }
+}
 
 sub get_nicklist {
     my ($server) = @_;
@@ -201,11 +240,13 @@ sub process_message {
     my ($server, $nick, $text) = @_;
     my $is_root = ($nick eq $remcon_root);
 
-    if ($listen_root || !$is_root) {
-        if (nick_exists($server, $remcon_admin)) {
-            $server->send_message($remcon_admin, "$nick: $text", 1);
-        } elsif (!$is_root) {
-            unshift(@awaylog, "$nick: $text");
+    if (only_if_detached_screen()) {
+        if ($listen_root || !$is_root) {
+            if (nick_exists($server, $remcon_admin)) {
+                $server->send_message($remcon_admin, "$nick: $text", 1);
+            } elsif (!$is_root) {
+                unshift(@awaylog, "$nick: $text");
+            }
         }
     }
 }
@@ -228,8 +269,10 @@ sub nicklist_remove {
 
     if ($remcon_autoaway_msg) {
         if ($nick->{'nick'} eq $remcon_admin) {
-            foreach my $server (Irssi::servers()) { 
-                $server->command("away $remcon_autoaway_msg");
+            if (only_if_detached_screen()) {
+                foreach my $server (Irssi::servers()) { 
+                    $server->command("away $remcon_autoaway_msg");
+                }
             }
         }
     }
@@ -242,6 +285,7 @@ sub remcon_start {
     $remcon_channel = Irssi::settings_get_str('remcon_channel');
     $remcon_root = Irssi::settings_get_str('remcon_root');
     $remcon_autoaway_msg = Irssi::settings_get_str('remcon_autoaway_msg');
+    $remcon_detect_attached_screen = Irssi::settings_get_bool('remcon_detect_attached_screen');
 
     if ($status == STATUS_STARTED) {
         print "Remcon already started!";
@@ -288,4 +332,5 @@ Irssi::settings_add_str('remcon', 'remcon_admin', 'remcon');
 Irssi::settings_add_str('remcon', 'remcon_channel', '&bitlbee');
 Irssi::settings_add_str('remcon', 'remcon_root', 'root');
 Irssi::settings_add_str('remcon', 'remcon_autoaway_msg', '');
+Irssi::settings_add_bool('remcon', 'remcon_detect_attached_screen', 1);
 
